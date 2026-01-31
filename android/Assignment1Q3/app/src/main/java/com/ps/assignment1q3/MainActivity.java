6eac647ebaeb24bc0165f63e537d86f313d4f173
sandip.cs <sandip.csmajors@gmail.com>
2026-01-31 06:55:39 +0530

    android updates

--- android/Assignment1Q3/app/src/main/java/com/ps/assignment1q3/MainActivity.java
+++ android/Assignment1Q3/app/src/main/java/com/ps/assignment1q3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ps.assignment1q3;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
@@ -18,75 +19,31 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    EditText etName, etEmail, etMobile, etPassword, etAge;
-    Button btnRegister;
-    ExpandableListView expandableListView;
-    List<String> groupList;
-    Map<String, List<String>> childMap;
+    EditText etFirstName, etLastName, etMobile, etEmail;
+    Button btnSubmit;
 
-    List<String> userRecords;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        etName = findViewById(R.id.etName);
-        etEmail = findViewById(R.id.etEmail);
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
         etMobile = findViewById(R.id.etMobile);
-        etPassword = findViewById(R.id.etPassword);
-        etAge = findViewById(R.id.etAge);
-        btnRegister = findViewById(R.id.btnRegister);
-        expandableListView = findViewById(R.id.records);
+        etEmail = findViewById(R.id.etEmail);
+        btnSubmit = findViewById(R.id.btnSubmit);
 
-        groupList = new ArrayList<>();
-        childMap = new HashMap<>();
-        userRecords = new ArrayList<>();
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
-
-        btnRegister.setOnClickListener(v -> {
-
-            String name = etName.getText().toString();
-            String email = etEmail.getText().toString();
-            String mobile = etMobile.getText().toString();
-            String password = etPassword.getText().toString();
-            String age = etAge.getText().toString();
-
-            if(name.isEmpty() || email.isEmpty() || mobile.isEmpty()
-                    || password.isEmpty() || age.isEmpty()) {
-
-                Toast.makeText(this,
-                        "All fields required",
-                        Toast.LENGTH_SHORT).show();
-                return;
+                Toast.makeText(
+                        MainActivity.this,
+                        "Registered successfully",
+                        Toast.LENGTH_SHORT
+                ).show();
             }
-
-            String record =
-                    "Name: " + name +
-                            "\nEmail: " + email +
-                            "\nMobile: " + mobile +
-                            "\nAge: " + age;
-
-            userRecords.add(record);
-            Toast.makeText(this,
-                    "Registered Successfully",
-                    Toast.LENGTH_SHORT).show();
-            clearFields();
-
         });
     }
 
-    private void clearFields() {
-        etName.setText("");
-        etEmail.setText("");
-        etMobile.setText("");
-        etPassword.setText("");
-        etAge.setText("");
-    }
 }
