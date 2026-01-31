380218213e3de10e3d85f1f1b71d79d604b9de4f
sandip.cs <sandip.csmajors@gmail.com>
2026-01-31 07:33:17 +0530

    Android updates

--- android/Assginment2Q3/app/src/main/java/com/ps/assginment2q3/MainActivity.java
+++ android/Assginment2Q3/app/src/main/java/com/ps/assginment2q3/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ps.assginment2q3;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,24 +11,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    EditText editText;
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        textView = findViewById(R.id.textView);
+        editText=findViewById(R.id.editText);
+        submit=findViewById(R.id.submit);
+        submit.setOnClickListener(v -> {
 
-        String receivedMsg =
-                getIntent().getStringExtra("msg");
+            String message = editText.getText().toString();
 
-        textView.setText(receivedMsg);
+            Intent intent =
+                    new Intent(MainActivity.this, MainActivity2.class);
+
+            intent.putExtra("msg", message);
+
+            startActivity(intent);
+        });
+
     }
 }
